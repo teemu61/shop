a1e4457bcc9fad9f63eb7e5e818fe28c19b728d2
Teemu Jäppinen <teemu.jappinen@alfame.com>
2020-04-24 12:05:59 +0300

    implement dropdown menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { LoginComponent } from './login/login.component';
       { path: 'login', component: LoginComponent},
       { path: 'admin/products', component: AdminOrdersComponent},
       { path: 'admin/orders', component: AdminOrdersComponent}
-    ])
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
