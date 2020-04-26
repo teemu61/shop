fc62b2b48cbe8fb9c7f0c30c878a03694cb32c93
Teemu Jäppinen <teemu.jappinen@alfame.com>
2020-04-26 19:56:37 +0300

    add route-redirect for login/logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,15 @@ export class AuthService {
 
   user$: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) { 
+  constructor(public afAuth: AngularFireAuth, private route: ActivatedRoute) { 
     this.user$ = afAuth.authState;
   }
 
   login() {
     console.log("login called...")
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+    
     var provider = new firebase.auth.GoogleAuthProvider();
     this.afAuth.auth.signInWithRedirect(provider);
   }
