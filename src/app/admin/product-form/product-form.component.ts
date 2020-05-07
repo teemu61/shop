ae40b75e81cdfe9e050b3efc4cbceaa28c99565b
Teemu Jäppinen <teemu.jappinen@alfame.com>
2020-05-07 11:49:38 +0300

    add new product with image to db

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -11,12 +12,16 @@ export class ProductFormComponent {
 
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService, 
+    private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
    }
 
    save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
    }
 
 
