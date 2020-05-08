002473336f43e77277aa1497721b6c8dcb10ee88
Teemu Jäppinen <teemu.jappinen@alfame.com>
2020-05-08 19:05:51 +0300

    add filtering of products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,19 +1,41 @@
 import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent  {
-  products$;
+export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) { 
-    this.products$ = productService.getAll();
+
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+
+    categoryService: CategoryService) {
+
+    productService.getAll().subscribe(products => {
+      
+      this.products = products;
+
+      /* filtering can not be done before products are fetched */
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+    });
+
     this.categories$ = categoryService.getAll();
-  }
 
+  }
 }
