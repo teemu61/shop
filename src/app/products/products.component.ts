f6c2faf65a411e83849a92647ef169031a7fad74
Teemu Jäppinen <teemu.jappinen@alfame.com>
2020-05-11 09:14:11 +0300

    refactor products.component.ts to use switchMap

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,8 +1,10 @@
+import { switchMap } from 'rxjs/operators';
 import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
+
 
 @Component({
   selector: 'app-products',
@@ -22,18 +24,19 @@ export class ProductsComponent {
 
     categoryService: CategoryService) {
 
-    productService.getAll().subscribe(products => {
-      
-      this.products = products;
-
-      /* filtering can not be done before products are fetched */
-      route.queryParamMap.subscribe(params => {
+    productService.getAll().pipe(
+      /* muunna Observable<Product> --> Observable<QueryParamMap> */
+      switchMap(products => {
+        this.products = products;
+        return route.queryParamMap;
+      }))
+      .subscribe(params => {
         this.category = params.get('category');
         this.filteredProducts = (this.category) ?
           this.products.filter(p => p.category === this.category) :
           this.products;
       });
-    });
+
 
     this.categories$ = categoryService.getAll();
 
